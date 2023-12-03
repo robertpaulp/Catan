0861fc637aabc056f119f0d9f3ff024689ea5b81
Alexandru Trifu-Urzică <alexandru.trifu2712@stud.acs.upb.ro>
2023-12-03 23:08:22 +0200

    changed settlement instance attributes

--- Scripts/board_elements/settlement.py
+++ Scripts/board_elements/settlement.py
@@ -2,38 +2,20 @@ import pygame
 from Scripts.constants import *
 from Scripts.base_game_cls import HexagonTile
 
-
-def prepare_board_surfaces(window, nodes):
-	""" Places possible settlement position surfaces on the map
-
-	:param window:
-	:param nodes: Hexagon nodes
-	:return: Dictionary containing (surface, position) elements
-	"""
-
-	settlement_locations = {}
-
-	for node in nodes:
-		surface = pygame.Surface((SETTLEMENT_SPRITE * 2, SETTLEMENT_SPRITE * 2), pygame.SRCALPHA)
-		hover_area = pygame.draw.circle(surface, (255, 0, 0, 50), (SETTLEMENT_SPRITE, SETTLEMENT_SPRITE), 10, 0)
-		position = tuple(map(lambda x: x - SETTLEMENT_SPRITE, list(node)))
-
-		settlement_locations[surface] = position
-
-		window.blit(surface, position)
-		pygame.display.update()
-
-	return settlement_locations
+settlement_img = pygame.Surface((SETTLEMENT_SPRITE * 3, SETTLEMENT_SPRITE * 3), pygame.SRCALPHA)
 
 
 class Settlement:
-	def __init__(self, placement_node: tuple, window):
+	settlements = []
+
+	def __init__(self, window, position):
 		""" Places settlement on the map
 
-		:param placement_node: Position of the desired settlement
 		"""
 
-		self.center = placement_node
+		self.image = pygame.Surface((SETTLEMENT_SPRITE * 3, SETTLEMENT_SPRITE * 3), pygame.SRCALPHA)
+		self.rect = self.image.get_rect(center=position)
+		self.position = position
 
 		self.__draw_settlement(window)
 
@@ -43,4 +25,34 @@ class Settlement:
 		:param window:
 		:return:
 		"""
-		pygame.draw.circle(window, (255, 50, 255), self.center, SETTLEMENT_SPRITE, 0)
+
+		# pygame.draw.circle(window, (255, 50, 255), self.center, SETTLEMENT_SPRITE, 0)
+
+	@classmethod
+	def prepare_board_surfaces(cls, window, nodes: tuple, opacity: int):
+		""" Places possible settlement position surfaces on the map
+
+		:param window: Window surface display
+		:param nodes: Hexagon nodes
+		:return: Dictionary containing (surface, position) elements
+		"""
+
+		settlement_locations = {}
+
+		for node in nodes:
+			settlement = Settlement(window, node)
+			# settlement.image = pygame.Surface((SETTLEMENT_SPRITE * 3, SETTLEMENT_SPRITE * 3), pygame.SRCALPHA)
+
+			cls.settlements.append(settlement)
+
+			settlement.image.set_alpha(0)
+			hover_area = pygame.draw.circle(settlement.image, (255, 0, 0, 255),
+			                                (SETTLEMENT_SPRITE, SETTLEMENT_SPRITE), SETTLEMENT_SPRITE, 0)
+			position = tuple(map(lambda x: x - SETTLEMENT_SPRITE, list(node)))
+
+			settlement_locations[settlement.image] = position
+
+			window.blit(settlement.image, position)
+			pygame.display.update()
+
+		return settlement_locations
