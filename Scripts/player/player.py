397176f383e0abf586ffcfab5b0a84b09ff5e7cd
Alexandru Trifu-Urzică <alexandru.trifu2712@stud.acs.upb.ro>
2023-12-04 22:08:50 +0200

    added acquire_cards method

--- Scripts/player/player.py
+++ Scripts/player/player.py
@@ -6,12 +6,22 @@ from Scripts.base_game_cls import HexagonTile
 class Player:
 	def __init__(self):
 		# Gameplay attributes
-		self.cards = []  # List of (card, quantity) tuples
+		self.cards = {}  # Dictionary of cards: key = resource, value = number of cards
 		self.settlements = []
 		self.roads = []
 
-		self.points = 0
+		self.win_points = 0
 
-	def acquire_card(self, card):
-		self.cards.append(card)
+	def acquire_cards(self, dice_roll):
+		""" Acquires cards based on dice roll and settlement position
+
+		:param dice_roll: Dice roll value
+		:return: None
+		"""
+
+		for settlement in self.settlements:
+			# If settlement is placed next to a hexagon with the dice roll associated to it, acquire resources
+			if dice_roll in settlement.dice_rolls:
+				for resource in settlement.resources:
+					self.cards[resource] += 1
 
