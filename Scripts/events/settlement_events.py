331bc7b6b99f5e009be63d42d8c9c9c1bb34b03d
Alexandru Trifu-Urzică <alexandru.trifu2712@stud.acs.upb.ro>
2023-12-07 01:03:19 +0200

    added error for insufficient resources

--- Scripts/events/settlement_events.py
+++ Scripts/events/settlement_events.py
@@ -38,6 +38,12 @@ class SettlementEventHandler:
 		for settlement in settlements:
 			# If mouse was released on the settlement we prepared, draw it
 			if settlement.rect.collidepoint(mouse_pos) and settlement.prepared_for_placement is True:
+				# Check if placement is possible
+				if Settlement.placement_is_possible(player) is False:
+					SettlementEventHandler.__error_popup(window)
+					settlement.prepared_for_placement = False
+					return
+
 				# Draw settlement sprite and change state
 				settlement.draw_settlement(window, player.color)
 				settlement.prepared_for_placement = False
@@ -48,6 +54,28 @@ class SettlementEventHandler:
 				player.settlements.append(settlement)
 				sprites.add(settlement)
 
+				# Spend resources
+				SettlementEventHandler.__buy_settlement(player)
+
 			# If mouse was not released on the settlement we prepared, reset its state
 			elif settlement.prepared_for_placement is True:
 				settlement.prepared_for_placement = False
+
+	@staticmethod
+	def __buy_settlement(player):
+		""" Spend resources to buy settlement
+
+		:param player:
+		:return:
+		"""
+
+		player.cards["Wood"] -= 1
+		player.cards["Brick"] -= 1
+		player.cards["Sheep"] -= 1
+		player.cards["Wheat"] -= 1
+
+	@staticmethod
+	def __error_popup(window):
+		err_image = pygame.image.load("../Assets/Sprites/saracie_error.png")
+		err_image = pygame.transform.scale(err_image, (400, 100))
+		window.blit(err_image, (0, 0))
